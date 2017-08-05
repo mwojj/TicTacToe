f93036d994cc757f75f6c4c1fe9fe68cce0248f0
mwojj <30746796+mwojj@users.noreply.github.com>
2017-08-05 16:31:34 +0200

    Runnable version of this app

--- src/Application.java
+++ src/Application.java
@@ -4,10 +4,7 @@ public class Application {
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello, I want to start create my new app - TTT");
-		
-		System.out.println("v0.1");
-		
+			
 		SwingUtilities.invokeLater(new Runnable(){
 
 			@Override
